d04596ab9c3b2ba975a0da32e811ce1b7bbc0284
SaraTuset <s.tuset.2020@alumnos.urjc.es>
2024-10-15 13:48:42 +0200

    Plantilla recetas valoración lista hasta que se cargue

--- public/js/recipe.js
+++ public/js/recipe.js
@@ -1,19 +1,31 @@
 $(document).ready(function(){
-    //Actualizar media = función devuelve media y cambia id="rated"
-$("input[type='radio']").click(function(){
-    //<cambiar a media
-        var sim =  $("input[type='radio']:checked").val(); 
-    //alert(sim);
-    if (sim<3) {
-        $('.myratings').css('color','red'); 
-        $(".myratings").text(sim);
-    }else{
-        $('.myratings').css('color','green');
-        $(".myratings").text(sim);
-    }
- });
+    $("input[type='radio']").click(function(){
+        //<cambiar a media
+            var sim =  actualizarMedia($("input[type='radio']:checked").val()); 
+        //alert(sim);
+        if (sim<3) {
+            $('.myratings').css('color','red'); 
+            $(".myratings").text(sim);
+        }else{
+            $('.myratings').css('color','green');
+            $(".myratings").text(sim);
+        }
+    });
 
 
 });
 
-//Habría que hacer función Actualizar media
+//Actualizar media = función devuelve media y cambia id="rated"
+async function actualizarMedia(valor){
+    const media = await fetch('/nuevaValoracion', 
+        { method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body:valor}) //En servidor debería hacer y guardar la media
+
+    let text = document.getElementById('rated');
+    text.innerHTML='Gracias por tu valoración: '+valor
+
+    return media
+}
