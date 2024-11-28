46866e21e78a70887592deabfb3099a7f1defb9a
Cristian Teijeiro Parga <c.teijeiro.2022@alumnos.urjc.es>
2024-11-28 12:39:18 +0100

    Refactor navigation components and enhance recipe display; add new styles for preview recipes

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,22 +1,23 @@
 window.onload = function () {
-    const username = '{{username}}';
+    const username = "{{username}}";
     const loginNav = document.getElementById("login_nav");
+    const loginNavMobile = document.getElementById("login_nav_mobile");
 
-    if (username) {
-        loginNav.innerHTML = `
+    if (username != "{{username}}") {
+        loginNav.innerHTML = loginNavMobile.innerHTML = `
                 <span class="navbar_username">${username}</span>
+                <button class="logout_button" onclick="location.href='/logout'">Cerrar sesión</button>
             `;
     } else {
-        loginNav.innerHTML = `
-                <button class="login_button" onclick="location.href='/login'">
+        loginNav.innerHTML = loginNavMobile.innerHTML = `
+                <button class="login_button menuItem" style="margin-bottom: 10px" onclick="location.href='/login'">
                     <i class="fa-solid fa-arrow-right-to-bracket" style="margin-right: 5px;"></i>
                     Iniciar Sesión
                 </button>
-                <button class="register_button" onclick="location.href='/register'">
+                <button class="register_button menuItem" onclick="location.href='/register'">
                     <i class="fa-solid fa-user-plus" style="margin-right: 5px;"></i>
                     Registrarse
                 </button>
             `;
     }
 }
-
